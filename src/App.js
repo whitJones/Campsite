5ffdae8035c96cf021c8e10cd3c1be9086c579e9
Whit Jones <wjones0219@gmail.com>
2023-01-14 13:35:59 -0700

    stateUse

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import logo from './logo.svg';
 import './App.css';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-        hello nucamp!
+        <Header />
+        <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='contact' element={<ContactPage />}/>
+            <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        </Routes>
+        <Footer />
     </div>
   );
 }
---
+++ src/features/campsites/campsitesSlice.js
@@ -0,0 +1,14 @@
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+
+export const selectAllCampsites = () => {
+    return CAMPSITES;
+};
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
+};
+
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(Math.random()*CAMPSITES.length)];
+// }
